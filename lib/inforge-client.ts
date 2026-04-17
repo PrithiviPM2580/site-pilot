63a22971e6470385c4f822a81c16fe2f64a36ca3
Prithivi <prithivi@users.noreply.github.com>
2026-04-17 15:11:09 +0545

    feat: update insforge client configuration for environment variables

--- lib/inforge-client.ts
+++ lib/inforge-client.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@insforge/sdk";
 
 export const insforge = createClient({
-  baseUrl: process.env.NEXT_PUBLIC_INSFORGE_BASE_URL!,
-  anonKey: process.env.NEXT_PUBLIC_INSFORGE_ANON_KEY!,
+  baseUrl:
+    process.env.INSFORGE_BASE_URL ||
+    "https://b4qvihfq.ap-southeast.insforge.app",
+  anonKey: process.env.INSFORGE_API_KEY!,
 });
