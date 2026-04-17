6425f2833cb9f4baea769e0553b48d15c346e59f
Prithivi <prithivi@users.noreply.github.com>
2026-04-18 02:47:42 +0545

    feat: add CanvasControls component for zoom and tool mode management

---
+++ components/canvas/canvas-controls.tsx
@@ -0,0 +1,77 @@
+import { TOOL_MODE_ENUM, ToolModeType } from "@/constants";
+import { Button } from "../ui/button";
+import { HandIcon, MinusIcon, MousePointer, PlusIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Separator } from "../ui/separator";
+
+type CanvasControlsProps = {
+  zoomIn: () => void;
+  zoomOut: () => void;
+  zoomPercent: number;
+  toolMode: ToolModeType;
+  setToolMode: (mode: ToolModeType) => void;
+};
+
+function CanvasControls({
+  zoomIn,
+  zoomOut,
+  zoomPercent,
+  toolMode,
+  setToolMode,
+}: CanvasControlsProps) {
+  return (
+    <div className="absolute bottom-10 -translate-x-1/2 left-1/2 flex items-center gap-2 rounded-full border bg-card py-1.5 px-4 shadow-md text-foreground">
+      <div className="flex items-center gap-1">
+        <Button
+          size={"icon-sm"}
+          variant={"ghost"}
+          className={cn(
+            "rounded-full cursor-pointer text-inherit hover:bg-secondary",
+            toolMode === TOOL_MODE_ENUM.SELECT && "bg-secondary",
+          )}
+          onClick={() => setToolMode(TOOL_MODE_ENUM.SELECT)}
+        >
+          <MousePointer />
+        </Button>
+        <Button
+          size={"icon-sm"}
+          variant={"ghost"}
+          className={cn(
+            "rounded-full cursor-pointer text-inherit hover:bg-secondary",
+            toolMode === TOOL_MODE_ENUM.HAND && "bg-secondary",
+          )}
+          onClick={() => setToolMode(TOOL_MODE_ENUM.HAND)}
+        >
+          <HandIcon />
+        </Button>
+      </div>
+      <Separator orientation="vertical" className="h-5" />
+      <div className="flex items-center gap-1">
+        <Button
+          size={"icon-sm"}
+          variant={"ghost"}
+          className={cn(
+            "rounded-full cursor-pointer text-inherit hover:bg-secondary",
+          )}
+          onClick={() => zoomOut()}
+        >
+          <MinusIcon />
+        </Button>
+        <div className="min-w-10 text-center text-sm">{zoomPercent}%</div>
+        <Button
+          size={"icon-sm"}
+          variant={"ghost"}
+          className={cn(
+            "rounded-full cursor-pointer text-inherit hover:bg-secondary",
+          )}
+          onClick={() => zoomIn()}
+        >
+          <PlusIcon />
+        </Button>
+        <div className="min-w-10 text-center text-sm">{zoomPercent}%</div>
+      </div>
+    </div>
+  );
+}
+
+export default CanvasControls;
