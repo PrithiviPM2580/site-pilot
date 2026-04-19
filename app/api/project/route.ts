da4ca5b699eb617c0c1b997b3cd533930ea40798
Prithivi <prithivi@users.noreply.github.com>
2026-04-19 13:58:28 +0545

    feat: implement project creation logic with title generation in POST endpoint

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -1,3 +1,4 @@
+import { generateProjectTitle } from "@/app/action/action";
 import { getAuthServer } from "@/lib/inforge-server";
 import { UIMessage } from "ai";
 import { NextRequest, NextResponse } from "next/server";
@@ -14,6 +15,36 @@ export async function POST(request: NextRequest) {
 
     if (!user?.id)
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    let { data: project, error: projectError } = await insforge.database
+      .from("projects")
+      .select("id,title")
+      .eq("slugId", slugId)
+      .single();
+
+    if (!project) {
+      console.log("Creating new project");
+      const lastMessage = messages[messages.length - 1];
+      const messageText = lastMessage?.parts.find(
+        (part) => part.type === "text",
+      )?.text as string;
+      const title = await generateProjectTitle(messageText);
+      const { data: newProject, error } = await insforge.database
+        .from("projects")
+        .insert([
+          {
+            title,
+            slugId,
+            userId: user.id,
+          },
+        ])
+        .select()
+        .single();
+
+      if (error) throw error;
+      if (!newProject) throw new Error("Failed to create project");
+      project = newProject;
+    }
   } catch (error) {
     return NextResponse.json(
       { error: "Internal Server Error" },
